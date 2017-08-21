a42a56f58ca5da11840bc4179f843889bc70508e
Sourav Kundu <Sourav Kundu@Sourav-PC>
2017-08-21 21:20:34 +0530

    Permutation of a string

---
+++ SapientTest/src/com/ANZ/interview/FindCombinaiton.java
@@ -0,0 +1,32 @@
+package com.ANZ.interview;
+
+import java.util.ArrayList;
+
+public class FindCombinaiton {
+
+	public static void main(String[] args) {
+		ArrayList<String> outputList =   findCombinations("abc");
+		System.out.println(outputList);
+
+	}
+	public static ArrayList<String>   findCombinations(String inut){
+		ArrayList<String> combinations = new ArrayList<>();
+		if(inut.length()<=0) return null;
+		if(inut.length()==1) {
+			combinations.add(inut);
+			return combinations;
+		}
+		
+		for(int i =0;i<inut.length();i++){
+			char ch  = inut.charAt(i);
+			String firstPart =  inut.substring(0,i) ;  
+			String secondPart = inut.substring(i+1);
+			//System.out.println("i="+i+" firstPart="+firstPart + " secondPart="+secondPart);
+			ArrayList<String> tempList= findCombinations(firstPart+secondPart);
+			for(String s:tempList){
+				combinations.add(ch+s);
+			}
+		}
+		return combinations;
+	}
+}
